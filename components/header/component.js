c3a5fb5dbbe5bef46a3a43d7452133484facf41e
barbara-chaves <barbarabarcellos90@gmail.com>
2023-11-15 15:59:29 +0100

    update style

---
+++ components/header/component.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import HeaderMenu from './menu';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header">
+        <div className="header__logo">
+          <img
+            className="logo"
+            src={`${process.env.BASE_PATH ?? ''}/images/mongabay-logo-white.png`}
+            alt="Mongabay"
+          />
+        </div>
+        <div className="header__content">
+          <p>SATELLITE TOOL</p>
+          <HeaderMenu />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
